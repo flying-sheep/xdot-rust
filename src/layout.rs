472dea086aa4ea5ca36bafb28c9ca761bf44504e
Philipp A <flying-sheep@web.de>
2023-02-02 22:00:21 -0500

    made optional

---
+++ src/layout.rs
@@ -0,0 +1,83 @@
+use graphviz_rust;
+use nom::Finish;
+use graphviz_rust::{
+    cmd::{CommandArg, Format, Layout},
+    dot_structures::{Attribute, Graph, Id},
+    printer::PrinterContext,
+};
+
+mod graph_ext;
+
+use crate::{ShapeDraw, XDotError, parse};
+use self::graph_ext::{Elem, GraphExt};
+
+pub fn layout_and_draw(graph: Graph) -> Result<Vec<ShapeDraw>, XDotError> {
+    let mut ctx = PrinterContext::default();
+    let layed_out = graphviz_rust::exec(
+        graph,
+        &mut ctx,
+        vec![
+            CommandArg::Layout(Layout::Dot),
+            CommandArg::Format(Format::Xdot),
+        ],
+    )?;
+    // println!("{}", &layed_out);
+    let graph = graphviz_rust::parse(&layed_out).map_err(|msg| XDotError::ParseDot(msg))?;
+    let shapes = graph
+        .iter_elems()
+        .map(handle_elem)
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>();
+    Ok(shapes)
+}
+
+const ATTR_NAMES: [&str; 6] = [
+    "_draw_", "_ldraw_", "_hdraw_", "_tdraw_", "_hldraw_", "_tldraw_",
+];
+
+fn handle_elem(elem: Elem) -> Result<Vec<ShapeDraw>, nom::error::Error<&str>> {
+    let attributes: &[Attribute] = match elem {
+        Elem::Edge(edge) => edge.attributes.as_ref(),
+        Elem::Node(node) => node.attributes.as_ref(),
+    };
+    let mut shapes = vec![];
+    for attr in attributes.iter() {
+        if let Id::Plain(ref attr_name) = attr.0 {
+            if !ATTR_NAMES.contains(&attr_name.as_str()) {
+                continue;
+            }
+            if let Id::Escaped(ref attr_val_raw) = attr.1 {
+                let attr_val = dot_unescape(attr_val_raw)?;
+                dbg!(&attr_name, &attr_val);
+                let mut new = parse(&attr_val)?;
+                shapes.append(&mut new);
+            }
+        }
+    }
+    Ok(shapes)
+}
+
+fn dot_unescape(input: &str) -> Result<&str, nom::error::Error<&str>> {
+    use nom::{
+        bytes::complete::{tag, take_while},
+        combinator::eof,
+        sequence::{delimited, terminated},
+    };
+    // TODO: actually unescape
+    let (_, inner) = terminated(
+        delimited(tag("\""), take_while(|c| c != '\\' && c != '\"'), tag("\"")),
+        eof,
+    )(input)
+    .finish()?;
+    Ok(inner)
+}
+
+#[test]
+fn test_dot_unescape() {
+    assert_eq!(dot_unescape("\"\""), Ok(""));
+    assert_eq!(dot_unescape("\"xy\""), Ok("xy"));
+    assert!(dot_unescape("\"\"\"").is_err());
+    assert!(dot_unescape("\"\\\"").is_err()); // so far no actual escape support
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,17 @@
-use graph_ext::{Elem, GraphExt};
-use graphviz_rust as gv;
-use gv::{
-    cmd::{CommandArg, Format, Layout},
-    dot_structures::{Attribute, Graph, Id},
-    printer::PrinterContext,
-};
-use nom::Finish;
+
 use thiserror::Error;
 
-mod graph_ext;
 mod xdot;
+#[cfg(feature = "layout")]
+mod layout;
 
 pub use xdot::{
     parse, Ellipse, ExternalImage, FontCharacteristics, Pen, Points, PointsType, Rgba, Shape,
     ShapeDraw, Style, Text, TextAlign,
 };
+
+#[cfg(feature = "layout")]
+pub use layout::layout_and_draw;
 
 #[derive(Error, Debug)]
 pub enum XDotError {
@@ -33,75 +30,4 @@ impl From<nom::error::Error<&str>> for XDotError {
         }
         .into()
     }
-}
-
-pub fn layout_and_draw(graph: Graph) -> Result<Vec<ShapeDraw>, XDotError> {
-    let mut ctx = PrinterContext::default();
-    let layed_out = gv::exec(
-        graph,
-        &mut ctx,
-        vec![
-            CommandArg::Layout(Layout::Dot),
-            CommandArg::Format(Format::Xdot),
-        ],
-    )?;
-    // println!("{}", &layed_out);
-    let graph = gv::parse(&layed_out).map_err(|msg| XDotError::ParseDot(msg))?;
-    let shapes = graph
-        .iter_elems()
-        .map(handle_elem)
-        .collect::<Result<Vec<_>, _>>()?
-        .into_iter()
-        .flatten()
-        .collect::<Vec<_>>();
-    Ok(shapes)
-}
-
-const ATTR_NAMES: [&str; 6] = [
-    "_draw_", "_ldraw_", "_hdraw_", "_tdraw_", "_hldraw_", "_tldraw_",
-];
-
-fn handle_elem(elem: Elem) -> Result<Vec<ShapeDraw>, nom::error::Error<&str>> {
-    let attributes: &[Attribute] = match elem {
-        Elem::Edge(edge) => edge.attributes.as_ref(),
-        Elem::Node(node) => node.attributes.as_ref(),
-    };
-    let mut shapes = vec![];
-    for attr in attributes.iter() {
-        if let Id::Plain(ref attr_name) = attr.0 {
-            if !ATTR_NAMES.contains(&attr_name.as_str()) {
-                continue;
-            }
-            if let Id::Escaped(ref attr_val_raw) = attr.1 {
-                let attr_val = dot_unescape(attr_val_raw)?;
-                dbg!(&attr_name, &attr_val);
-                let mut new = parse(&attr_val)?;
-                shapes.append(&mut new);
-            }
-        }
-    }
-    Ok(shapes)
-}
-
-fn dot_unescape(input: &str) -> Result<&str, nom::error::Error<&str>> {
-    use nom::{
-        bytes::complete::{tag, take_while},
-        combinator::eof,
-        sequence::{delimited, terminated},
-    };
-    // TODO: actually unescape
-    let (_, inner) = terminated(
-        delimited(tag("\""), take_while(|c| c != '\\' && c != '\"'), tag("\"")),
-        eof,
-    )(input)
-    .finish()?;
-    Ok(inner)
-}
-
-#[test]
-fn test_dot_unescape() {
-    assert_eq!(dot_unescape("\"\""), Ok(""));
-    assert_eq!(dot_unescape("\"xy\""), Ok("xy"));
-    assert!(dot_unescape("\"\"\"").is_err());
-    assert!(dot_unescape("\"\\\"").is_err()); // so far no actual escape support
 }
