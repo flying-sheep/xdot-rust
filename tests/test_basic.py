c2183acb952048108c7412d55c7932764cc69749
Philipp A <flying-sheep@web.de>
2023-03-12 18:51:23 +0100

    auto black

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,6 +3,14 @@ from types import ModuleType
 import xdot_rs
 
 
-def test_import_structure():
+def test_import_structure_base():
+    pass
+
+
+def test_import_structure_draw():
     assert isinstance(xdot_rs.draw, ModuleType)
-    assert isinstance(xdot_rs.draw, ModuleType)
+
+
+def test_import_structure_shapes():
+    assert isinstance(xdot_rs.shapes, ModuleType)
+    assert isinstance(xdot_rs.shapes.Shape, type)
